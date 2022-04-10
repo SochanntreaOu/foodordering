ae4f21b5d984dfb7d347f109d53987a552b8568f
Sochanntrea Ou <ousochanntrea18@kit.edu.kh>
2022-04-10 19:45:48 +0700

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'foodorder'`, () => {
+  it(`should have as title 'foodmine-course'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('foodorder');
+    expect(app.title).toEqual('foodmine-course');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('foodorder app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('foodmine-course app is running!');
   });
 });
